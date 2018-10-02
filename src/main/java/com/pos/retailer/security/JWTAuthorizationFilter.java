c438aeb4f84b2f541a1da6d0a981e635b6bac83e
Sarbe <sarbe97@gmail.com>
2018-10-03 05:08:36 +0530

    auth error

--- src/main/java/com/pos/retailer/security/JWTAuthorizationFilter.java
+++ src/main/java/com/pos/retailer/security/JWTAuthorizationFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,6 +16,9 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pos.retailer.exception.ApiError;
 
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
@@ -25,18 +29,33 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain)
 			throws IOException, ServletException {
+		try {
+			String header = req.getHeader(SecurityConstants.AUTHORIZATION);
+			if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+				filterChain.doFilter(req, res);
+				return;
+			}
 
-		String header = req.getHeader(SecurityConstants.AUTHORIZATION);
-		if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+			UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+
+			SecurityContextHolder.getContext().setAuthentication(authentication);
 			filterChain.doFilter(req, res);
-			return;
+		} catch (Exception e) {
+			System.err.println(e.getClass());
+			ApiError apiError = new ApiError("EXC_0099", HttpStatus.BAD_REQUEST, e.getMessage(),
+					e.getMessage());
+			
+			res.setStatus(HttpStatus.BAD_REQUEST.value());
+			res.getWriter().write(convertObjectToJson(apiError));
 		}
+	}
 
-		UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
-
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		filterChain.doFilter(req, res);
-
+	private String convertObjectToJson(Object object) throws JsonProcessingException {
+		if (object == null) {
+			return null;
+		}
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsString(object);
 	}
 
 	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
